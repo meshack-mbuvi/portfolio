766850b471738bfd855ed9c098624967e4af7e93
Meshack Mbuvi <meshmbuvi@gmail.com>
2024-07-15 03:04:56 +0300

    feat: initial build of portfolio website
    - Set up project structure and installed dependencies
    - Implemented responsive layout and navigation
    - Added sections for About, Projects, Experience, and Contact
    - Integrated initial styling with CSS/SCSS

---
+++ data/index.ts
@@ -0,0 +1,214 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Currently building a Cryptocurrency Exchange App",
+    description: "Decentralized crypto exchange React app",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "rounded-2xl absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/dex.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Syndicate dashboard",
+    des: "Startups and Fortune 500 companies use Syndicate’s APIs and infrastructure to build highly scalable, user-friendly, web3-powered products with their existing stack.",
+    img: "/syndicate.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/fm.svg"],
+    link: "https://syndicate.io",
+  },
+  {
+    id: 2,
+    title: "Government Marketplace",
+    des: "Developed a government marketplace for Kenya, enabling service providers to offer their services directly to the government. This centralized platform promotes transparency and equality, ensuring all service providers have an equal opportunity to bid on government contracts.",
+    img: "/psc-marketplace.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "",
+  },
+  {
+    id: 3,
+    title: "Promptopia",
+    des: "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.",
+    img: "/promptopia.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg"],
+    link: "https://promptopia-seven-pi.vercel.app/",
+  },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "https://github.com/adrianhajdin/iphone",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Meshack is a skilled and thoughtful engineer, during our time together at Syndicate he built e2e features quickly and efficiently while always thinking about reusability in the code. He is always looking to help and find ways to meaningfully contribute to the product.He would be an asset to any engineering team",
+    name: "Sam Mason de Caires",
+    title: "Staff Engineer @syndicate",
+    avatar: "/sam.jpeg",
+  },
+  {
+    quote:
+      "I had the pleasure to work with Meshack for a year at Andela collaborating on a high-traffic product.\
+       He expertly filled the role of a technical team lead and that of a backend developer.\
+        As the product manager getting informative and honest updates from him weekly was always a joy.\
+         His communication was to the point and understandable.\
+          I was particularly impressed by his ability to mentor over 5 developers and still manage to produce quality work, personally.\
+           He was the greatest developer to work with in terms of quality output, timely stakeholder management and autonomy in his work.\
+            I would recommend him to any employer.",
+    name: "Harrison Kamau",
+    title: "Full Stack Engineer",
+    avatar: "/kamau.jpeg",
+  },
+  {
+    quote:
+      "Meshack is a reliable, talented and hardworking engineer. Over the period I worked with him, he consistently delivered very high-quality outputs in time.\
+       He takes time to understand the problem domain, plan out his work and execute it.\
+        He is a good communicator, possesses excellent stakeholder management skills and always exceeds expectations in his deliverables.\
+         Meshack is a good fit for any team that is passionate about delivering high-quality outputs. I highly recommend him.",
+    name: "Micah Oriaso",
+    title: "Software Engineer",
+    avatar: "/micah.jpeg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "syndicate",
+    img: "/syndicate.png",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Web3 Developer",
+    desc: "Led the development of innovative Web3 applications, focusing on decentralized technologies. Created user-friendly interfaces, integrated blockchain features, and ensured seamless interaction with smart contracts. Specialized in React, Web3.js, and Ethereum, delivering robust and responsive DApps.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Fullstack Developer",
+    desc: "Engineered comprehensive web applications by managing both frontend and backend development. Utilized technologies like React, Node.js, and Typescript to create scalable and efficient solutions. Implemented RESTful APIs, designed user-friendly interfaces, and ensured seamless integration between client and server-side functionalities. Delivered robust, high-performance applications tailored to user needs.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Fullstack Developer",
+    desc: "As the Technical Team Lead for Andela Meals, an internal product used daily by 1200 users, I spearheaded the development and delivery of innovative solutions. I guided a team of talented engineers, leveraging my expertise in software architecture and project management to facilitate seamless collaboration and ensure project alignment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies. Led the seamless integration of a customized steps-bot with the company’s primary platform, enhancing efficiency and user experience. Collaborated closely with cross-functional teams to analyze requirements, design workflows, and implement robust API integrations.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/meshack-mbuvi",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/meshack-mbuvi/",
+  },
+];
